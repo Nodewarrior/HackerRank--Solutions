9e37f74dcecb4a0394fabd089a3a2c917ac9ca29
Nodewarrior <monish161992@gmail.com>
2017-03-19 02:27:06 -0400

    Update Euler2.java

--- Euler2.java
+++ Euler2.java
@@ -1,38 +1,34 @@
+import java.io.*;
 import java.util.*;
-
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        long n = 0;
-        for (int a0 = 0; a0 < t; a0++) {
-            n = in.nextLong();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+            System.out.println(Solution.fibonacci(n));
+           
         }
-       long sum = fibonacci(n);
-        System.out.println(sum);
-
+        
+        
     }
-    static  long fibonacci(long n){
-        int n1,n2,n3,sum=0;
-        for(int i=0; i<=n; i++){
-
-            n1 = 1; n2 = 2; n3 = 0;
-            n3 = n1 + n2;
+    
+    public static long fibonacci(long x){
+        long n1, n2=0, n3=1;
+        long sum = 0;
+        while(n3<x){
             n1 = n2;
             n2 = n3;
-            if(n>2){
-                while(n2%2 == 0) {
-                    sum = sum + n2;
-                }
-            } else {
-                sum = 2;
+            n3 = n1 + n2;
+            if(n3%2 == 0 && n3 < x){
+                sum += n3;
             }
-
         }
         return sum;
     }
-
 }
-
