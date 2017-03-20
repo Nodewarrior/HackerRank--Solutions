a4b03c660f103a85573c29d9dc5726f76a7e4801
Nodewarrior <monish161992@gmail.com>
2017-03-20 13:51:52 -0400

    Update and rename Euler2.java to Projecteuler2-fibo.java

--- Projecteuler2-fibo.java
+++ Projecteuler2-fibo.java
@@ -9,26 +9,24 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            long n = in.nextLong();
-            System.out.println(Solution.fibonacci(n));
-           
+        long n = 0;
+        for (int a0 = 0; a0 < t; a0++) {
+            n = in.nextLong();
+             System.out.println(fibonacci(n));
         }
-        
-        
+
     }
-    
+
     public static long fibonacci(long x){
-        long n1, n2=0, n3=1;
-        long sum = 0;
-        while(n3<x){
+        long sum = 0, n1 = 1, n2 = 2, n3 = n1 + n2;
+        while(n3 < x){
+            if(n3 % 2 ==0) {
+                sum += n3;
+            }
             n1 = n2;
             n2 = n3;
             n3 = n1 + n2;
-            if(n3%2 == 0 && n3 < x){
-                sum += n3;
-            }
         }
-        return sum;
+        return sum + 2;
     }
 }
